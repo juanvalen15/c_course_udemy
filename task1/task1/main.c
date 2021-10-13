a71ffdeb248aeb003f5f795d9e493a1fde02f15a
juanvalen15 <juanvalen15@gmail.com>
2021-10-13 22:49:34 +0200

    static variable
    the static variable is only incremented by 1 with the call of the function

--- task1/task1/main.c
+++ task1/task1/main.c
@@ -7,45 +7,34 @@
 
 #include <stdio.h>
 
-int random_number_generator(int *seed);
+int random_number_generator(void);
 
-extern int global_seed; // outside of main and random_number_generator functions
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     int random_number;
-    int main_seed; // main_seed resides in a memory location
     
-    global_seed = 3;
+    random_number = random_number_generator();
+    printf("rand = %d\n", random_number);
 
-    main_seed = 10;
+    random_number = random_number_generator();
+    printf("rand = %d\n", random_number);
     
-    random_number = random_number_generator(&main_seed);
-    
-    printf("global seed = %d\n", global_seed);
-    
-    printf("main_seed = %d, randn = %d\n", main_seed, random_number);
     getc(stdin);
     
     return 0;
 }
 
-int global_seed;
 
-int random_number_generator(int *seed) // seed resides in a memory location
+
+int random_number_generator(void) // seed resides in a memory location
 {
     int number;
+    static int seed = 0;
+    seed++;
     
-    printf("seed before initialisation = %d\n", *seed);
+    printf("seed = %d\n", seed);
     
-    *seed = 20;
-    
-    printf("seed = %d\n", *seed);
-    
-    
-    global_seed = 20;
-    
-    number = global_seed * 1103515245 + 12345;
-    //number = *seed * 1103515245 + 12345;
+    number = seed * 1103515245 + 12345;
     return (unsigned int)(number/65536) % 32768;
 }
