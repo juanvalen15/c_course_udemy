9971af50982fbe79364559fcf7c2415054342e5d
juanvalen15 <juanvalen15@gmail.com>
2021-10-13 22:29:57 +0200

    updated main.c
    added pointers to random number generator. now we need to pass a pointer as a parameters

--- task1/task1/main.c
+++ task1/task1/main.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 
-int random_number_generator(int seed);
+int random_number_generator(int *seed);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -16,7 +16,7 @@ int main(int argc, const char * argv[]) {
     
     main_seed = 10;
     
-    random_number = random_number_generator(main_seed);
+    random_number = random_number_generator(&main_seed);
     
     printf("main_seed = %d, randn = %d\n", main_seed, random_number);
     getc(stdin);
@@ -24,16 +24,16 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
-int random_number_generator(int seed) // seed resides in a memory location
+int random_number_generator(int *seed) // seed resides in a memory location
 {
     int number;
     
-    printf("seed before initialisation = %d\n", seed);
+    printf("seed before initialisation = %d\n", *seed);
     
-    seed = 20;
+    *seed = 20;
     
-    printf("seed = %d\n", seed);
+    printf("seed = %d\n", *seed);
     
-    number = seed * 1103515245 + 12345;
+    number = *seed * 1103515245 + 12345;
     return (unsigned int)(number/65536) % 32768;
 }
