2f715ea1e9417a4023c5c8133a92fe040cdbc7e2
juanvalen15 <juanvalen15@gmail.com>
2021-10-13 22:36:02 +0200

    global variable
    global variable example

--- task1/task1/main.c
+++ task1/task1/main.c
@@ -9,14 +9,21 @@
 
 int random_number_generator(int *seed);
 
+int global_seed; // outside of main and random_number_generator functions
+
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     int random_number;
     int main_seed; // main_seed resides in a memory location
     
+    global_seed = 3;
+
     main_seed = 10;
     
     random_number = random_number_generator(&main_seed);
+    
+    printf("global seed = %d\n", global_seed);
     
     printf("main_seed = %d, randn = %d\n", main_seed, random_number);
     getc(stdin);
@@ -34,6 +41,10 @@ int random_number_generator(int *seed) // seed resides in a memory location
     
     printf("seed = %d\n", *seed);
     
-    number = *seed * 1103515245 + 12345;
+    
+    global_seed = 20;
+    
+    number = global_seed * 1103515245 + 12345;
+    //number = *seed * 1103515245 + 12345;
     return (unsigned int)(number/65536) % 32768;
 }
